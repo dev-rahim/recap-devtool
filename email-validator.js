4328eb0b50d969b097e3911eceacae62acdc6831
dev-rahim <rahimuddin5915@gmail.com>
2022-10-03 17:54:28 +0600

    exploring email validation with regular expretion

--- email-validator.js
+++ email-validator.js
@@ -2,14 +2,19 @@ console.log('I am ok');
 
 const mailValidation = () => {
     const userMail = document.getElementById('user-mail').value;
+    const div = document.getElementById('validation-result');
     let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     if (pattern.test(userMail)) {
-        const small = document.createElement('small');
-        small.classList.add('text-success')
-
-        document.getElementById('header').appendChild()
+        div.innerHTML = `
+        <small class="text-success">Your mail is valid</small>
+        `;
+        document.getElementById('header').appendChild(div)
     }
     else {
-
+        div.textContent = '';
+        div.innerHTML = `
+        <small class="text-danger">Your mail is not valid</small>
+        `;
+        document.getElementById('header').appendChild(div)
     }
 }
